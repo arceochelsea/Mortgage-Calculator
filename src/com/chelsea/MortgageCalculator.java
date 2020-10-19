20f3810daeda5190f858524e9fde5105723378be
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-10-19 14:57:39 -0400

    Initial Commit.

---
+++ src/com/chelsea/MortgageCalculator.java
@@ -0,0 +1,16 @@
+package com.chelsea;
+
+import java.util.Scanner;
+
+public class MortgageCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principle");
+        long principle = scanner.nextLong();
+        System.out.println("Your principle is " + principle);
+
+
+    }
+
+}
