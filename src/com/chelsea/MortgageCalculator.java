b00f1ea94ae86b83498f30753b926d99a306d43a
Chelsea <62972835+arceochelsea@users.noreply.github.com>
2020-10-26 00:08:59 -0400

    Refactored main method, extracted new method to calculate mortgage

--- src/com/chelsea/MortgageCalculator.java
+++ src/com/chelsea/MortgageCalculator.java
@@ -1,4 +1,4 @@
-//For each question we want to validate the value that the user enters. If invalid we want to ask the same question again.
+//Refactor main method and extract a new method to calculate the mortgage
 
 package com.chelsea;
 
@@ -8,15 +8,10 @@ import java.text.NumberFormat;
 public class MortgageCalculator {
 
     public static void main(String[] args) {
-        
-        //consts
-        int monthsInAYear = 12;
-        int percentage = 100;
-        
-        //these were removed out of the code block so it is able to be accessed globally
+
         int principal = 0; 
-        float monthlyInterest = 0;
-        int numOfPayments = 0;
+        float annualInterest = 0;
+        byte years = 0;
 
         Scanner scanner = new Scanner(System.in);
 
@@ -30,39 +25,38 @@ public class MortgageCalculator {
 
         while (true) {
             System.out.print("Annual Interest Rate: ");
-            float annualInterest = scanner.nextFloat();
-            if (annualInterest >= 1 && annualInterest <= 30) { //if valid value, calculate monthlyinterest then break out of the loop if not print error
-                monthlyInterest = annualInterest / percentage / monthsInAYear;
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30)//if valid value, calculate monthlyinterest then break out of the loop if not print error
                 break;
-            }
             System.out.println("Enter a value between 1 and 30");
         }
 
         while (true) {
             System.out.print("Period: (Years): ");
-            byte years = scanner.nextByte();
-            if (years >= 1 && years <= 30) { //if valid calculate numofpayments then break, if not print error
-                numOfPayments = years * monthsInAYear;
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30) //if valid calculate numofpayments then break, if not print error
                 break;
-            }
             System.out.println("Enter a value between 1 and 30.");
         }
 
-        /*
-        M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1]
-        P = principal loan amount
-
-        i = monthly interest rate
-
-        n = number of months required to repay the loan
-        */
-
-        //this will work the same because consts were placed outside of local scope (inside code block) to outside of it!
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numOfPayments)) / (Math.pow(1 + monthlyInterest, numOfPayments) - 1);
-
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
     }
+    
+    public static double calculateMortgage(int principal, float annualInterest, byte years) {
+        
+        //all the code to calculate the mortgage is in one location now 
+        final byte monthsInAYear = 12;
+        final byte percentage = 100;
+        
+        float monthlyInterest = annualInterest / percentage / monthsInAYear;
+        short numOfPayments = (short)(years * monthsInAYear);
+        
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numOfPayments)) / (Math.pow(1 + monthlyInterest, numOfPayments) - 1);
+        
+        return mortgage;
 
 }
 
